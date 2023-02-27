f92c4d980d4eeea56ac169a1cfdb1791011e7a45
wangwenkai <wangwenkai01@gridsum.com>
2023-02-27 18:02:41 +0800

    feat(axios封装): 对请求方法的拦截

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -3,14 +3,22 @@ import axios from 'axios'
 import type { AxiosInstance } from 'axios'
 
 import { RequestConfig, RequestInterceptors } from './type'
+import { ElLoading } from 'element-plus'
+import { LoadingInstance } from 'element-plus/lib/components/loading/src/loading.js'
 
+const DEFAULT_LOADING = true
 class Request {
   instance: AxiosInstance
   interceptor?: RequestInterceptors
+  loading?: LoadingInstance
+  showLoading: boolean
   constructor(config: RequestConfig) {
     this.instance = axios.create(config)
 
     this.interceptor = config?.interceptor
+    console.log(config)
+
+    this.showLoading = DEFAULT_LOADING
 
     this.instance.interceptors.request.use(
       this.interceptor?.requestInterceptor,
@@ -25,6 +33,11 @@ class Request {
     this.instance.interceptors.request.use(
       (config) => {
         console.log('请求拦截')
+        if (this.showLoading) {
+          this.loading = ElLoading.service({
+            text: '请稍候'
+          })
+        }
 
         return config
       },
@@ -34,6 +47,7 @@ class Request {
     )
     this.instance.interceptors.response.use(
       (response) => {
+        this.loading?.close()
         const data = response.data
         if (!data.success) {
           alert('请求失败')
@@ -41,6 +55,7 @@ class Request {
         return response.data
       },
       (error) => {
+        this.loading?.close()
         if (error.response.status !== 200) {
           alert(error.code)
         }
@@ -49,17 +64,42 @@ class Request {
       }
     )
   }
-  async request(config: RequestConfig) {
-    if (config.interceptor?.requestInterceptor) {
-      config = config.interceptor?.requestInterceptor(config as any)
+  request<T>(payload: RequestConfig): Promise<T> {
+    if (payload.interceptor?.requestInterceptor) {
+      payload = payload.interceptor?.requestInterceptor(payload as any)
     }
-    // return this.instance.request(config)
-    if (config.interceptor?.responseInterceptor) {
-      let response = await this.instance.request(config)
-      response = config.interceptor?.responseInterceptor(response)
-      return response
+
+    if (payload.showLoading === false) {
+      this.showLoading = payload.showLoading
     }
-    return this.instance.request(config)
+
+    return new Promise((resolve, reject) => {
+      this.instance
+        .request<any, T>(payload)
+        .then((response) => {
+          if (payload.interceptor?.responseInterceptor) {
+            response = payload.interceptor?.responseInterceptor(response)
+          }
+          this.showLoading = DEFAULT_LOADING
+          return resolve(response)
+        })
+        .catch((err) => {
+          this.showLoading = DEFAULT_LOADING
+          return reject(err)
+        })
+    })
+  }
+  get<T>(payload: RequestConfig) {
+    return this.instance.request<T>({ ...payload, method: 'GET' })
+  }
+  post<T>(payload: RequestConfig) {
+    return this.instance.request<T>({ ...payload, method: 'POST' })
+  }
+  put<T>(payload: RequestConfig) {
+    return this.instance.request<T>({ ...payload, method: 'PUT' })
+  }
+  delete<T>(payload: RequestConfig) {
+    return this.instance.request<T>({ ...payload, method: 'DELETE' })
   }
 }
 
--- src/service/request/type.ts
+++ src/service/request/type.ts
@@ -1,20 +1,24 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import {
   InternalAxiosRequestConfig,
-  AxiosRequestConfig,
-  AxiosResponse
+  AxiosRequestConfig
+  // AxiosResponse
 } from 'axios'
+
 interface RequestInterceptors {
   requestInterceptor?: (
     config: InternalAxiosRequestConfig
   ) => InternalAxiosRequestConfig
   requestInterceptorCatch?: (error: any) => any
-  responseInterceptor?: (config: AxiosResponse) => AxiosResponse
+  // responseInterceptor?: (config: AxiosResponse) => AxiosResponse
+  responseInterceptor?: (config: any) => any
+
   responseInterceptorCatch?: (error: any) => any
 }
 
 interface RequestConfig extends AxiosRequestConfig {
   interceptor?: RequestInterceptors
+  showLoading?: boolean
 }
 
 export { RequestConfig, RequestInterceptors }
